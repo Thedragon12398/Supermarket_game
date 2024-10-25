ed34a1eb271e7f0b9cd083e1e8a1ac4e516b29bb
Tavier Futrell <109624625+Thedragon12398@users.noreply.github.com>
2024-10-25 14:07:54 -0400

    Game Over screen works

--- Assets/Death Scene/MainMenu.cs
+++ Assets/Death Scene/MainMenu.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
-    public void OnButtonClick(ClickEvent evt)
+    public void Main()
     {
-        SceneManager.LoadScene("Title Screen");
+        SceneManager.LoadScene(0);
     }
 }

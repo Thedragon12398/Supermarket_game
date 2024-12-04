4b2d4ac322fe2904191d2f20c83e467952821746
Tavier Futrell <109624625+Thedragon12398@users.noreply.github.com>
2024-12-04 14:29:24 -0500

    Trying to add pause menu

---
+++ Assets/Assets/Player/CharacterController/Scripts/Pause.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseMenuCanvas;
+    public bool IsPaused;
+
+    void Start()
+    {
+        PauseMenuCanvas.SetActive(false);
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+    public void pause()
+    {
+        PauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0;
+        IsPaused = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1;
+        IsPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Title Screen");
+        IsPaused = false;
+    }
+}
